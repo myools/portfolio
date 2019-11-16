96f0a9c5dc6d954bcc4362c6dfce9ebcc5468035
Zach Mueller <zachrm614@gmail.com>
2019-11-16 00:20:16 -0600

    updated files

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+//variables to keep track of popup element states and window width changes
 var popupVisible = false;
+var aboutPopupVisisble = false;
 var screenWidth = $(window).width();
 
 //handle burger icon when clicked
@@ -24,7 +26,7 @@ function handleBurger(){
         }
    })
 }
-//remove popout menu when screen width changes
+//remove popout menu when screen width changes to avoid issues
 function removePopupIfWidthChanges(){
     $(window).on('resize', function() {
         if ($(this).width() != screenWidth) {
@@ -37,19 +39,47 @@ function removePopupIfWidthChanges(){
 //remove popout if screen is clicked
 function removePopupIfScreenClicked(){
     
-    $('body').on('click', function(){
+    $('body').on('click', function(event){
         if (popupVisible == true){
         $('.link-wrapper-popup').remove();
         popupVisible = false;
         console.log(popupVisible);
+        event.stopPropagation();
         }
     })
 }
 
+//handle get to know me button which generates more about me content on click
+//removes about me content when on click when visible
+function handleGetToKnowMe(){
+    $('body').on('click', '.about-me', function(){
+        if (aboutPopupVisisble === false){
+        $('.about-me').after(`<p class="popup-p">In my personal life I enjoy:</p>
+        <ul class="popup-ul">
+        <li>Spending time with my wife & family</li>
+        <li>Video games</li>
+        <li>Music of all sorts</li>
+        <li>Being outdoors</li>
+        <li>My cat</li>
+        </ul>
+        `)
+        aboutPopupVisisble = true;
+        return;
+        }
 
+        if (aboutPopupVisisble === true){
+            $('.popup-p').remove();
+            $('.popup-ul').remove();
+            aboutPopupVisisble = false;
+        }
+    })
 
+}
+
+//main function to run page
 function handlePage(){
     removePopupIfWidthChanges();
     handleBurger();
     removePopupIfScreenClicked();
+    handleGetToKnowMe();
 }
